c5e99953d49da9996fc30a4d4b1342871406b3c6
Leonid-davydov <lenya-davydov@mail.ru>
2022-06-14 15:08:12 +0300

    level 17 servlets

---
+++ src/main/java/by/incubator/application/servlets/ViewDiagnosticServlet.java
@@ -0,0 +1,27 @@
+package by.incubator.application.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@WebServlet(name = "/viewDiagnostic")
+public class ViewDiagnosticServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("cars", vehicleTypeService.getVehicles());
+        RequestDispatcher dispatcher = this.getServletContext()
+                .getRequestDispatcher("/jsp/viewDiagnosticJSP.jsp");
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/main/java/by/incubator/application/servlets/ViewInfoServlet.java
@@ -0,0 +1,32 @@
+package by.incubator.application.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@WebServlet(name = "/info")
+public class ViewInfoServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        request.setAttribute("cars",
+                vehicleTypeService.getVehicles().stream().filter(vehicleDto -> id =
+                vehicleDto.getId()).collect(Collectors.toList()));
+        request.setAttribute("rents", vehicleTypeService.getRent(id));
+        RequestDispatcher dispatcher = this.getServletContext()
+                .getRequestDispatcher("/jsp/viewCarInfoJSP.jsp");
+        dispatcher.forward(request, response);
+    }
+}
